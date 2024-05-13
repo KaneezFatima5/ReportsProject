e582ff478303fcd26d901fb0c1673d7f267182ca
Kaneez Fatima <kaneezfatimarajper55@gmail.com>
2024-05-13 11:19:04 +0500

    developed get functionalities

--- src/main/java/Service/IndividualsService.java
+++ src/main/java/Service/IndividualsService.java
@@ -3,8 +3,13 @@ package Service;
 import DTOs.IndividualDto;
 import Entities.Individuals;
 import Repository.IndividualsRepo;
+import org.hibernate.annotations.NotFound;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IndividualsService {
@@ -14,7 +19,7 @@ public class IndividualsService {
         this.individualsRepo=individualsRepo;
         this.modelMapper=modelMapper;
     }
-    public Boolean createIndividualM(IndividualDto individualDto){
+    public Boolean createIndividual(IndividualDto individualDto){
         Individuals individual=new Individuals();
         individual.setName(individualDto.getName());
         individual.setAge(individualDto.getAge());
@@ -26,5 +31,18 @@ public class IndividualsService {
         }catch (Exception ex){
             throw new RuntimeException("Can not create individual");
         }
+    }
+    public IndividualDto getIndividual(int id){
+        Individuals individual=individualsRepo.findById(id).orElseThrow(() -> new RuntimeException("individual not found"));
+        IndividualDto individualDto=modelMapper.map(individual, IndividualDto.class);
+        return individualDto;
+    }
+    public List<IndividualDto> getAll(){
+        List<Individuals> individuals=this.individualsRepo.findAll();
+        List<IndividualDto> individualDtos=individuals.stream().map(x -> this.modelMapper.map(x, IndividualDto.class)).collect(Collectors.toList());
+        return individualDtos;
+    }
+    public void deleteIndividual(int id){
+        
     }
 }
